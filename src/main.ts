18d07890617406beac10fcd00a2f2780fd6e068c
Mika <mpeersha@ucsc.edu>
2023-10-07 17:17:36 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,10 @@ const gameName = "Whale game";
 document.title = gameName;
 
 const header = document.createElement("h1");
+const button = document.createElement("button");
+
 header.innerHTML = gameName;
+button.innerHTML = "🐳";
+
 app.append(header);
+app.append(button);
