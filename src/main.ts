704d4912b785dc2c4bd4a006d75a7e34cb5a36a0
Mika <mpeersha@ucsc.edu>
2023-10-07 20:52:33 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -11,20 +11,22 @@ function increaseCounter(change: number) {
   counter += change;
   counterText.innerHTML = `${Math.round(counter)} Whales`;
   // Disables/enables the button based on the units collected
-  counter >= 10
+  counter >= price[0]
     ? (shopButtonA.disabled = false)
     : (shopButtonA.disabled = true);
-  counter >= 100
+  counter >= price[1]
     ? (shopButtonB.disabled = false)
     : (shopButtonB.disabled = true);
-  counter >= 1000
+  counter >= price[2]
     ? (shopButtonC.disabled = false)
     : (shopButtonC.disabled = true);
 }
 
-// Step 6
+// Step 7
 let rate = 0;
-const purchase = [0, 0, 0]; // A, B, C
+const purchase = [0, 0, 0]; // How many times the player got A, B, C
+const price = [10, 100, 1000]; // Current prices of A, B, C
+
 // Takes args since the counter and rate need to be changed no matter what item is clicked
 function shop(itemType: number, itemRate: number, itemCost: number) {
   if (counter >= itemCost) {
@@ -32,14 +34,30 @@ function shop(itemType: number, itemRate: number, itemCost: number) {
     counter -= itemCost;
     rate += itemRate;
     purchase[itemType - 1] += 1;
+    price[itemType - 1] *= 1.15; // Increase price by a factor of 1.15
     // Update text
     growthRate.innerHTML = "Growth Rate: " + rate.toFixed(2) + " whales/sec";
-    itemA.innerHTML = "Item A: " + purchase[0].toFixed(2);
-    itemB.innerHTML = "Item B: " + purchase[1].toFixed(2);
-    itemC.innerHTML = "Item C: " + purchase[2].toFixed(2);
+    itemA.innerHTML =
+      "Item A: " +
+      purchase[0].toFixed(2) +
+      " (price: " +
+      price[0].toFixed(2) +
+      ")";
+    itemB.innerHTML =
+      "Item B: " +
+      purchase[1].toFixed(2) +
+      " (price: " +
+      price[1].toFixed(2) +
+      ")";
+    itemC.innerHTML =
+      "Item C: " +
+      purchase[2].toFixed(2) +
+      " (price: " +
+      price[2].toFixed(2) +
+      ")";
+    // Use the updated rate every second
+    setInterval(() => increaseCounter(rate), 1000);
   }
-  // Use the updated rate every second
-  setInterval(() => increaseCounter(rate), 1000);
 }
 
 const header = document.createElement("h1");
@@ -56,9 +74,9 @@ const shopButtonC = document.createElement("button");
 
 header.innerHTML = gameName;
 growthRate.innerHTML = "Rate: 0 whales/sec";
-itemA.innerHTML = "Item A: 0";
-itemB.innerHTML = "Item B: 0";
-itemC.innerHTML = "Item C: 0";
+itemA.innerHTML = "Item A: 0 (price: 10)";
+itemB.innerHTML = "Item B: 0 (price: 100)";
+itemC.innerHTML = "Item C: 0 (price: 1000)";
 
 counterText.innerHTML = `0 Whales`;
 
@@ -67,13 +85,13 @@ button.addEventListener("click", () => increaseCounter(1), false);
 
 shopButtonA.innerHTML = "A";
 shopButtonA.disabled = true;
-shopButtonA.addEventListener("click", () => shop(1, 0.1, 10), false);
+shopButtonA.addEventListener("click", () => shop(1, 0.1, price[0]), false);
 shopButtonB.innerHTML = "B";
 shopButtonB.disabled = true;
-shopButtonB.addEventListener("click", () => shop(2, 2, 100), false);
+shopButtonB.addEventListener("click", () => shop(2, 2, price[1]), false);
 shopButtonC.innerHTML = "C";
 shopButtonC.disabled = true;
-shopButtonC.addEventListener("click", () => shop(3, 50, 1000), false);
+shopButtonC.addEventListener("click", () => shop(3, 50, price[2]), false);
 
 app.append(header);
 app.append(growthRate);
