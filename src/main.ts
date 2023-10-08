ba44998084ea414854736e49ac72e77db70e0af3
Mika <mpeersha@ucsc.edu>
2023-10-07 17:18:34 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,21 @@ const gameName = "Whale game";
 
 document.title = gameName;
 
+let counter: number = 0;
+function increaseCounter() {
+  counter++;
+  counterText.innerHTML = `${counter} Whales`;
+}
+
 const header = document.createElement("h1");
 const button = document.createElement("button");
+const counterText = document.createElement("div");
 
 header.innerHTML = gameName;
 button.innerHTML = "🐳";
+counterText.innerHTML = `0 Whales`;
+button.addEventListener("click", increaseCounter, false);
 
 app.append(header);
 app.append(button);
+app.append(counterText);
